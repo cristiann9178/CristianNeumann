4c37df1cc60987e71fa187d0b6d20f3a8e87153b
Cristian Neumann <cristian9178@gmail.com>
2024-04-27 23:07:49 -0300

    backend ok

--- project/config/urls.py
+++ project/config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("empresa.urls")),
+    path('', include("core.urls")),
+    path("empresa/", include("empresa.urls"))
 ]
--- project/empresa/views.py
+++ project/empresa/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 from . import models
 
-#def home (request):
+def home (request):
+    query = models.Areas.objects.all()
+    contexto = {"areas": query}
+    return render(request, "empresa/index.html", context=contexto)
